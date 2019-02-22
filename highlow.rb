4f77cefb9d28c7205cbd9526c3fa38627000f05d
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-02-22 13:34:18 -0700

    Added low balance and adding more money during the game

--- highlow.rb
+++ highlow.rb
@@ -1,8 +1,9 @@
 require_relative 'deck'
 require_relative 'card'
-require 'pry'
 
 class HighLow < Deck
+
+  
   
 
   def from_wallet
@@ -24,12 +25,14 @@ class HighLow < Deck
   
   def higher_lower
     while @playagain != "N"
+    low_wallet
     @card2 = @cards.sample.rank.to_s
     puts "Is the next card Higher or Lower? H/L"
     @guess = gets.upcase.to_s.chomp
     @cardorder = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     @compare = @cardorder.index(@card2) > @cardorder.index(@card1)
     @compare2 = @cardorder.index(@card2) == @cardorder.index(@card1)
+    @compare3 = @cardorder.index(@card2) < @cardorder.index(@card1)
     case @guess
     when "H"
     puts "Your next card is: #{@card2}"
@@ -39,20 +42,20 @@ class HighLow < Deck
     elsif @compare == false
       if @compare2 == false
         @gamewallet -= @bet
-        puts "Incorrect, ,you now have $#{@gamewallet}"
+        puts "Incorrect, you now have $#{@gamewallet}"
       else
         puts "Push. No change in your wallet"
       end
     end
     when "L"
       puts "Your next card is: #{@card2}"
-      if @compare == false
+      if @compare3 == true
         @gamewallet += @bet
       puts "You are correct, you now have $#{@gamewallet}"
-      elsif @compare == true
+      elsif @compare3 == false
         if @compare2 == false
           @gamewallet -= @bet
-        puts "Incorrect, ,you now have $#{@gamewallet}"
+        puts "Incorrect, you now have $#{@gamewallet}"
         else
         puts "Push. No change in your wallet"
       end
@@ -60,15 +63,39 @@ class HighLow < Deck
     
     else
       puts "Entry Error. Is the next card Higher or Lower? H/L"
-   
-end
-@card1 = @card2
-@card2 = nil
-puts "Would you like to guess on another card?"
-@playagain = gets.upcase.to_s.chomp
-end
-  
-    puts "Goodbye, $#{@gamewallet} has been deposited back into your player wallet"
+    end
+    
+    @card1 = @card2
+    @card2 = nil
+    puts "Would you like to guess on another card?"
+    @playagain = gets.upcase.to_s.chomp
+    
+  end 
+  puts "Thanks for playing, $#{@gamewallet} has been put back into your player wallet"
   end
+end
+
+def add_more
+  puts "How much money would you like to add from your wallet?"
+  @moremoney = gets.to_i
+  @gamewallet += @moremoney
+  puts "You now have $#{@gamewallet} in your account"
+end
+
+def low_wallet
+  if @gamewallet - @bet < 0
+    puts "You do not have enough money to complete your next bet, would you like to deposit more money? Y/N"
+    @moremoney = gets.upcase.to_s.chomp
+    if @moremoney == "Y"
+    add_more
+    return
+    elsif @moremoney == "N"
+      puts "Thanks for playing, $#{@gamewallet} has been put back into your player wallet"
+      exit
+    end
+  else
+   return
+  end
+  
 end
 
