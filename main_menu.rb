3678cb88145f85909d9f63a49cae24c55a6b2bd8
David Sarkisian <dsarkisian@plansource.com>
2019-02-21 20:58:43 -0700

    Updated Main menu, updated walet

--- main_menu.rb
+++ main_menu.rb
@@ -1,7 +1,43 @@
 class MainMenu
 
-  def initialize
-    puts "MAIN MENU"
-  end
+  @@menu_options = [
+    'Create a player',
+    'Switch players',
+    'Exit to main menu'
+  ]
 
+def initialize
+  puts
+  puts "WELCOME TO OUR CASINO. PLEASE SELECT: "
+
+  @@menu_options.each_with_index do |option,i|
+    print " #{i+1}) #{option}"
+    puts
+  end
+end
+
+def choose_from_menu
+  
+  choice = gets.chomp.to_i
+  case choice 
+    when 1
+      puts 
+      puts "Enter new players name"
+      new_player_name = gets.chomp
+      puts "How much money is the player coming in with?"
+      balance = gets.to_i
+      player = Player.new(new_player_name, balance)
+      # wallet = Wallet.new(balance)
+    when 2
+      puts
+      puts "Enter player's name"
+      
+    when 3 
+      main_menu = MainMenu.new
+    else 
+      puts
+      puts "Invalid choice. Please try again"
+      initialize
+    end
+end
 end
--- player_menu.rb
+++ player_menu.rb
@@ -4,6 +4,7 @@ require_relative 'main_menu'
 
 
 class PlayerMenu 
+  attr_accessor :player
 
   @@menu_options = [
     'Play a Game',
@@ -11,21 +12,26 @@ class PlayerMenu
     'Exit to main menu'
   ]
 
-  def initialize
+  def initialize(name)
     puts
-    puts "PLAYER MENU. PLEASE SELECT: "
+    puts "Welcome, #{name}"
+    puts "PLEASE SELECT: "
 
     @@menu_options.each_with_index do |option,i|
       print " #{i+1}) #{option}"
       puts
     end
+  end
+
+  def choose_from_menu(balance)
 
     choice = gets.chomp.to_i
     case choice 
       when 1
-        player = Player.new
+        puts "select a game"
       when 2
-        wallet = Wallet.new
+        wallet = Wallet.new(balance)
+        wallet.display
       when 3 
         main_menu = MainMenu.new
       else 
