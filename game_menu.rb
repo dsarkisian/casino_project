de9692f96ba5b4a070a6b9dfbb4d2b47126be075
Brantley Eason <brantleyeason@Brantleys-MacBook-Pro.local>
2019-02-21 20:14:30 -0700

    add game menu

---
+++ game_menu.rb
@@ -0,0 +1,31 @@
+
+
+# Player name
+# See your bank account
+
+class GameMenu
+  def menu
+    puts "Select a game:"
+    puts '1) Slots'
+    puts '2) High/Low'
+    puts '3) exit'
+    puts "Good Luck!"
+  end
+
+  def user_selection
+   menu
+    choice = gets.to_i
+    case choice
+    when 1
+     slots
+    when 2
+      high_low
+    when 3
+      puts "Goodbye"
+      exit
+    else
+      puts 'invalid choice, please try again'
+  end
+end
+
+
